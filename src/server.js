4dc4ae7f005bba95df62571ea7eb7d84bd316390
Hayunitya <hayunitya549@gmail.com>
2025-09-17 00:20:43 +0700

    feat: add API endpoint to list and filter projects

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const projectRoutes = require('./routes/projectRoutes');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Routes utama
+app.use('/api/projects', projectRoutes);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
