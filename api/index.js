848e085691bda7a8d955184d94538fcf84404a50
fahmiirfanfaiz <fahmiirfanfaiz01@gmail.com>
2025-11-11 10:26:22 +0700

    feat: add Vercel serverless configuration

---
+++ api/index.js
@@ -0,0 +1,13 @@
+/**
+ * Vercel Serverless Function Entry Point
+ * This file is required for Vercel deployment to work properly
+ *
+ * Unlike traditional Express servers that use app.listen(),
+ * Vercel requires a serverless handler that processes each request
+ */
+
+const app = require("../src/app");
+
+// Export the Express app as a serverless function handler
+// Vercel will call this function for each incoming request
+module.exports = app;
