f04aa95bbf8eb6c11cf9506b153f3ac0332e056b
Moses Saidasdo Purba <145828744+saidasdo@users.noreply.github.com>
2025-11-16 23:59:44 +0700

    Remove authMiddleware from public competency routes

--- src/routes/competencyRoutes.js
+++ src/routes/competencyRoutes.js
@@ -5,9 +5,9 @@ const { authMiddleware, requireRole } = require('../middlewares/authMiddleware')
 const { validateCompetencyCreate, validateCompetencyUpdate } = require('../middlewares/competencyValidator');
 
 // Public routes (semua user yang sudah login bisa akses)
-router.get('/', authMiddleware, competencyController.getAllCompetencies);
-router.get('/categories', authMiddleware, competencyController.getCompetencyCategories);
-router.get('/by-category', authMiddleware, competencyController.getCompetenciesByCategory);
+router.get('/', competencyController.getAllCompetencies);
+router.get('/categories', competencyController.getCompetencyCategories);
+router.get('/by-category', competencyController.getCompetenciesByCategory);
 
 // Admin only routes
 router.post('/', authMiddleware, requireRole('admin'), validateCompetencyCreate, competencyController.createCompetency);
